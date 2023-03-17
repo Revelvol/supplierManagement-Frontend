f90db397972dc593b366309144c2852659d86664
julius sutandi <julius.sutandi@gmail.com>
2023-03-17 16:53:42 +0700

    added table react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,18 +4,22 @@ import { Routes, Route, Navigate} from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
 import { useIsAuthenticated } from 'react-auth-kit';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import ProtectedRoutes from './pages/protectedRoutes';
 import Register from './pages/register';
 import Profile from './pages/profile';
 import SupplierManagement from './pages/supplierManagement';
 
 
+const queryClient = new QueryClient()
+
 
 function App() {
 
   const isAuth = useIsAuthenticated(true);
   return (
-    <>
+    
+    <QueryClientProvider client={queryClient}>
         <Routes>
         <Route path='/login' element={isAuth() ? <Navigate to="/" /> : <Login />} />
         <Route path='/register' element={isAuth() ? <Navigate to="/" /> : <Register />}/>
@@ -27,7 +31,7 @@ function App() {
           <Route path='/supplier-management' element={<SupplierManagement/>}/>
         </Route>
       </Routes>
-  </>
+    </QueryClientProvider>
   );
 }
 
--- my-app/src/pages/supplierManagement.js
+++ my-app/src/pages/supplierManagement.js
@@ -1,8 +1,68 @@
+// import react table here 
+import { useQuery } from "react-query"
+import SupplierTables from "./tables/supplierTables"
+import React from "react"
+
+const supplierUrl ="http://ec2-54-199-2-15.ap-northeast-1.compute.amazonaws.com/api/suppliers/"
+
 
 function SupplierManagement () {
+    const columns = React.useMemo(
+        () => [
+          {
+            Header: 'Name',
+            columns: [
+              {
+                Header: 'First Name',
+                accessor: 'firstName',
+              },
+              {
+                Header: 'Last Name',
+                accessor: 'lastName',
+              },
+            ],
+          },
+          {
+            Header: 'Info',
+            columns: [
+              {
+                Header: 'Age',
+                accessor: 'age',
+              },
+              {
+                Header: 'Visits',
+                accessor: 'visits',
+              },
+              {
+                Header: 'Status',
+                accessor: 'status',
+              },
+              {
+                Header: 'Profile Progress',
+                accessor: 'progress',
+              },
+            ],
+          },
+        ],
+        []
+      )
+    const { isLoading, error, data } = useQuery('supplierData', () =>
+     fetch(supplierUrl).then(res =>
+       res.json()
+     )
+   )
+ 
+   if (isLoading) return 'Loading...'
+ 
+   if (error) return 'An error has occurred: ' + error.message
+
+    // ok data itu adalah array dengan index 0 adalah data yang paling atas 
+    
+    
     return (
         <div>
             supplier Management
+            <SupplierTables columns={NaN} data={NaN} /> 
         </div>
     )
 }
