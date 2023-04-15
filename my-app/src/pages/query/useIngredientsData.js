06e7de97ade866617746285c8a29616de75cf852
Revelvol <julius.sutandi@gmail.com>
2023-04-15 23:19:26 +0700

    add add Ingredient data query hook

--- my-app/src/pages/query/useIngredientsData.js
+++ my-app/src/pages/query/useIngredientsData.js
@@ -1,15 +1,38 @@
 import axios from "axios";
-import { useQueryClient, useQuery } from "react-query";
+import { useQueryClient, useQuery, useMutation } from "react-query";
 
 const fetchIngredientsData = () => {
+  /* Fetch all ingredients data  */
   return axios.get(
     "http://ec2-54-199-2-15.ap-northeast-1.compute.amazonaws.com/api/ingredients/"
   );
 };
 const fetchIngredientsDataSupplier = ({ queryKey }) => {
+  /* Fetch ingredient data based on supplier id */
   const supplierId = queryKey[1];
   return axios.get(
     `http://ec2-54-199-2-15.ap-northeast-1.compute.amazonaws.com/api/ingredients/?supplier_id=${supplierId}`
+  );
+};
+
+const addIngredientData = (data) => {
+  const payload = {
+    supplier: data.supplierId,
+    name: data.name,
+    price: data.price,
+    quantity: data.quantity,
+    is_used: data.is_used,
+    function: data.function, 
+    unit: data.unit, 
+  }
+  return axios.post(
+    "http://ec2-54-199-2-15.ap-northeast-1.compute.amazonaws.com/api/ingredients/", payload, {
+      headers: {
+        Authorization: data.token,
+        "Content-Type": "application/json",
+
+      }
+    }
   );
 };
 
@@ -23,4 +46,15 @@ export const useGetIngredientsData = (supplierId = null) => {
     ? fetchIngredientsDataSupplier
     : fetchIngredientsData;
   return useQuery(queryKey, queryFn);
+};
+
+export const useAddIngredientData = () => {
+  /* usequery hook to add ingredients  */
+  const queryClient = useQueryClient();
+
+  return useMutation(addIngredientData, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("ingredientsData");
+    },
+  });
 };
