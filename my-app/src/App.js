7d40dee73259642de27ffbdd739694bf77f3dd52
Revelvol <julius.sutandi@gmail.com>
2023-04-15 22:46:04 +0700

    delete document/edit table

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,52 +1,59 @@
-import './App.css';
-import React from 'react';
-import { Routes, Route, Navigate} from 'react-router-dom';
-import Home from './pages/pageHomeUser/home';
-import Login from './pages/pageHomeUser/login';
-import { useIsAuthenticated } from 'react-auth-kit';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import ProtectedRoutes from './pages/pageHomeUser/protectedRoutes';
-import Register from './pages/pageHomeUser/register';
-import Profile from './pages/pageHomeUser/profile';
-import SupplierManagement from './pages/pageSupplierManagement/supplierManagement';
-import EditSupplier from './pages/pageSupplierManagement/editSupplier';
+import "./App.css";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/pageHomeUser/home";
+import Login from "./pages/pageHomeUser/login";
+import { useIsAuthenticated } from "react-auth-kit";
+import { QueryClient, QueryClientProvider } from "react-query";
+import ProtectedRoutes from "./pages/pageHomeUser/protectedRoutes";
+import Register from "./pages/pageHomeUser/register";
+import Profile from "./pages/pageHomeUser/profile";
+import SupplierManagement from "./pages/pageSupplierManagement/supplierManagement";
+import EditSupplier from "./pages/pageSupplierManagement/editSupplier";
 
-import {ReactQueryDevtools} from 'react-query/devtools'
-import IngredientManagement from './pages/pageIngredientManagement/ingredientsManagement';
-import IngredientDocument from './pages/pageIngredientManagement/ingredientDocument';
-import EditDocument from './pages/pageIngredientManagement/editIngredient';
+import { ReactQueryDevtools } from "react-query/devtools";
+import IngredientManagement from "./pages/pageIngredientManagement/ingredientsManagement";
 
+import EditDocument from "./pages/pageIngredientManagement/editIngredient";
 
-
-const queryClient = new QueryClient()
-
+const queryClient = new QueryClient();
 
 function App() {
-
   const isAuth = useIsAuthenticated(true);
   return (
-   
     <QueryClientProvider client={queryClient}>
-        <Routes>
-        <Route path='/login' element={isAuth() ? <Navigate to="/" /> : <Login />} />
-        <Route path='/register' element={isAuth() ? <Navigate to="/" /> : <Register />}/>
-       
+      <Routes>
+        <Route
+          path="/login"
+          element={isAuth() ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={isAuth() ? <Navigate to="/" /> : <Register />}
+        />
+
         {/* Protected Routes starts from here */}
-        <Route element={<ProtectedRoutes auth={isAuth()}/>}>
-          <Route path='/' element={<Home />}/>
-          <Route path='/profile' element={<Profile/>}/>
-          <Route path='/supplier-management' element={<SupplierManagement/>}/>
-          <Route path='/supplier-management/edit/:supplierId'element={<EditSupplier/>} />
-          <Route path= '/ingredient-management' element={<IngredientManagement/>} />
-          <Route path= '/ingredient-management/document/:ingredientId' element={<IngredientDocument/>} />
-          <Route path= '/ingredient-management/edit/:ingredientId' element={<EditDocument/>} />
+        <Route element={<ProtectedRoutes auth={isAuth()} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/supplier-management" element={<SupplierManagement />} />
+          <Route
+            path="/supplier-management/edit/:supplierId"
+            element={<EditSupplier />}
+          />
+          <Route
+            path="/ingredient-management"
+            element={<IngredientManagement />}
+          />
+          <Route
+            path="/ingredient-management/edit/:ingredientId"
+            element={<EditDocument />}
+          />
         </Route>
       </Routes>
       <ReactQueryDevtools initialIsOpem={false} position="bottom-right" />
     </QueryClientProvider>
-   
   );
 }
-
 
 export default App;
