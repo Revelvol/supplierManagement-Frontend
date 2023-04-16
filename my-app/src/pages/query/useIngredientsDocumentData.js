64737ed385513ea317e1f4f8d9d992a755ac7867
Revelvol <julius.sutandi@gmail.com>
2023-04-16 18:22:30 +0700

    fix bug in fetch ingredient document usequery hook

--- my-app/src/pages/query/useIngredientsDocumentData.js
+++ my-app/src/pages/query/useIngredientsDocumentData.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useQueries } from "react-query";
 
-const fetchIngredientsDocumentData = async (ingredientId) => {
+const fetchIngredientsDocumentData = async ({ queryKey }) => {
+  const ingredientId = queryKey[1];
   const url = `http://ec2-54-199-2-15.ap-northeast-1.compute.amazonaws.com/api/ingredients/${ingredientId}/upload-document/`;
   const response = await axios.get(url);
   if (!response.ok) {
@@ -25,8 +26,10 @@ export const useGetIngredientsDocumentsData = (ingredientData) => {
   /* usequery hook get document ingredient data 
   with the coresponding ingredient data  */
 
-  return useQueries(ingredientData?.data.map((ingredient) => ({
-    queryKey: ["ingredientDocumentData", ingredient.id],
-    queryFn: fetchIngredientsDocumentData(ingredient.id)
-  })) || []);
+  return useQueries(
+    ingredientData?.data.map((ingredient) => ({
+      queryKey: ["ingredientDocumentData", ingredient.id],
+      queryFn: fetchIngredientsDocumentData,
+    })) || []
+  );
 };
