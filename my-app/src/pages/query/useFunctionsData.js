e5544daaa71a262f2b280d4af075c8887f30c3c2
Revelvol <julius.sutandi@gmail.com>
2023-04-18 23:20:08 +0700

    added intiial data to getfunction and unit hook

--- my-app/src/pages/query/useFunctionsData.js
+++ my-app/src/pages/query/useFunctionsData.js
@@ -7,10 +7,17 @@ const fetchFunctionsData = () => {
   );
 };
 
-
 export const useGetFunctionsData = () => {
-    /* React Query Hook to fetch functions Data  */
-  const queryKey = "functions"
-  const queryFn = fetchFunctionsData
-  return useQuery(queryKey, queryFn);
+  /* React Query Hook to fetch functions Data  */
+  const queryClient = useQueryClient();
+  const queryKey = "functions";
+  const queryFn = fetchFunctionsData;
+  return useQuery(queryKey, queryFn, {
+    initialData: () => {
+      const functionData = queryClient.getQueryData(queryKey);
+      if (functionData) {
+        return { data: functionData };
+      }
+    },
+  });
 };
--- my-app/src/pages/query/useUnitsData.js
+++ my-app/src/pages/query/useUnitsData.js
@@ -10,7 +10,18 @@ const fetchUnitsData = () => {
 
 export const useGetUnitsData = () => {
     /* React Query Hook to fetch units Data  */
+  const queryClient = useQueryClient();
   const queryKey = "units"
   const queryFn = fetchUnitsData
-  return useQuery(queryKey, queryFn);
+  return useQuery(queryKey, queryFn, {
+    initialData: () =>{
+      const unit = queryClient.getQueryData("units")
+      if(unit){
+        return {
+          data:unit
+        }
+      }
+      
+    }
+  });
 };
