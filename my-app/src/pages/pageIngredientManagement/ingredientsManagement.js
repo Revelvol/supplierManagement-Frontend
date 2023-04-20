b83b543c5b5546edc80df55ff98c3f0461dcafb2
Revelvol <julius.sutandi@gmail.com>
2023-04-20 18:59:38 +0700

    refractor ingredient management

--- my-app/src/pages/pageIngredientManagement/ingredientsManagement.js
+++ my-app/src/pages/pageIngredientManagement/ingredientsManagement.js
@@ -1,107 +1,34 @@
 import { useSuppliersData } from "../query/useSuppliersData";
-import { useGetIngredientsData } from "../query/useIngredientsData";
-import { useState, useMemo } from "react";
-import IngredientTables from "../tables/IngredientTables";
-import AddIngredientForm from "../forms/addIngredientForm";
-import { useGetIngredientsDocumentsData } from "../query/useIngredientsDocumentData";
 import { Link } from "react-router-dom";
 
 function IngredientManagement() {
-  const [ingredientTable, setIngredientTable] = useState(<div> </div>);
   const {
     isLoading: suppliersIsLoading,
     error: suppliersError,
     data: suppliersData,
   } = useSuppliersData();
 
-  const {
-    isLoading: ingredientsIsLoading,
-    error: ingredientsError,
-    data: ingredientsData,
-  } = useGetIngredientsData();
-
-  const documentQueries = useGetIngredientsDocumentsData(ingredientsData);
-
-  const ingredientsWithDocumentData = ingredientsData?.data.map(
-    /* map ingredient with document data */
-    (ingredient) => {
-      try {
-        const documents = documentQueries.find(
-          (query) => parseInt(query.data.ingredient) === parseInt(ingredient.id)
-        ).data;
-        const mergedData = {
-          ...ingredient,
-          ...documents,
-        };
-
-        delete mergedData.ingredient;
-        return mergedData;
-      } catch (err) {
-        // error happen here
-      }
-    }
-  );
 
   if (
-    suppliersIsLoading ||
-    ingredientsIsLoading ||
-    documentQueries.some((query) => query.isLoading)
+    suppliersIsLoading 
   ) {
     return "is loading .... ";
   }
 
   if (
-    suppliersError ||
-    ingredientsError ||
-    documentQueries.some((query) => query.error)
+    suppliersError 
   ) {
     let alertMessage = "";
     if (suppliersError) {
       alertMessage += suppliersError.message + " ";
     }
-    if (ingredientsError) {
-      alertMessage += ingredientsError.message + " ";
-    }
-    documentQueries.forEach((query) => {
-      if (query.error) {
-        alertMessage += query.error.message + " ";
-      }
-    });
+    
     return (
       <div className="alert alert-danger" role="alert">
         {alertMessage}
       </div>
     );
-  }
-
-  const handleSupplierClick = (event) => {
-    /* set the displayed table of ingredient 
-    based on the supplier clicked*/
-    const supplierId = event.target.parentNode.id;
-    const ingredientData = ingredientsWithDocumentData.filter((ingredient) => {
-      if (ingredient.supplier === parseInt(supplierId)) {
-        return ingredient;
-      } else {
-        return null;
-      }
-    });
-
-    if (ingredientData.length === 0) {
-      setIngredientTable(
-        <>
-          <span>There is no ingredient, add ingredient!!!</span>
-          <AddIngredientForm supplierId={supplierId} />
-        </>
-      );
-    } else {
-      setIngredientTable(
-        <>
-          <AddIngredientForm supplierId={supplierId} />
-          <IngredientTables data={ingredientData} />
-        </>
-      );
-    }
-  };
+  }  
 
   return (
     <div>
@@ -122,7 +49,6 @@ function IngredientManagement() {
           );
         })}
       </ul>
-      <div className="ingredientTable">{ingredientTable}</div>
     </div>
   );
 }
